188357ba81bab96909342e26970888696ae63450
AlexKir008 <kirshaav@yandex.ru>
2021-08-06 09:15:52 +0300

    2. Сервис - Кинотеатр [#3236 #186202] error correction

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,15 @@
 CREATE TABLE account (
                          id SERIAL PRIMARY KEY,
                          username VARCHAR NOT NULL,
-                         phone VARCHAR NOT NULL UNIQUE
-                         --email VARCHAR NOT NULL UNIQUE
+                         phone VARCHAR NOT NULL UNIQUE,
+                         email VARCHAR NOT NULL UNIQUE
 );
 
 CREATE TABLE ticket (
                         id SERIAL PRIMARY KEY,
                         row INT NOT NULL,
                         cell INT NOT NULL,
-                        account_id INT NOT NULL REFERENCES account(id)
-                        --session_id INT NOT NULL
+                        account_id INT NOT NULL REFERENCES account(id),
+                        session_id INT NOT NULL,
+                        unique (session_id, row, cell)
 );
